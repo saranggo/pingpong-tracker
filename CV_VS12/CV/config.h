a39806e0a2730af4facd7f576f9ae5384b014678
Prasanna <prasannakumar.0091@gmail.com>
2013-12-01 15:33:35 -0500

    ...

--- CV_VS12/CV/config.h
+++ CV_VS12/CV/config.h
@@ -3,8 +3,11 @@
 #define __PROJECT_CONFIG__
 
 #define DBG
-#define IMG_PATH "H:\\Private\\Fall 13\\Project\\pingpong-tracker\\trunk\\trial_images"
+#define IMG_PATH "/home/prasanna/opencv_workspace/pingpong-tracker/trial_images"
 #define USE_TEST_IMAGES
 #define _g_ 9.81
+#define CAMERA_MATRIX_RGB { {525, 0, 319.5},  {0, 525, 239.5},  {0, 0, 1}}
+#define CAMERA_MATRIX_DEPTH {{575.8175, 0, 314.5},  {0, 575.8175, 325.06},  {0, 0, 1}}
+#define RGB_TO_DEPTH {{1, 0, 0, -0.025},  {0, 1, 0, 0},  {0, 0, 1, 0}}
 
 #endif
---
+++ CV_VS12/CV/main_conv.cpp
@@ -0,0 +1,92 @@
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <cmath>
+#include "ProjectileEst.cpp"
+#include "toWorldConverter.cpp"
+#include "config.h"
+
+using namespace cv;
+using namespace std;
+
+extern void scalarFilter(const Mat&, Mat&, const Scalar&, const Scalar&,  bool);
+extern int detectEllipse(const Mat&, RotatedRect&, int, int, int);
+int getNextImage(Mat&, Mat&, int n=-1);
+
+bool pause1 = true;
+int main( int argc, char** argv )
+{
+	string windowResult1 = "Result1";
+	//namedWindow(windowResult1, CV_WINDOW_AUTOSIZE );
+	string windowResult2 = "Result2";
+	//namedWindow(windowResult2, CV_WINDOW_AUTOSIZE );
+
+	Mat src_bgr, src_dep;
+	getNextImage(src_bgr, src_dep, 18);
+	ProjectileEst esti; 
+	toWorldConverter conv;
+	while(getNextImage(src_bgr, src_dep))
+	{
+		/// color filter
+		Mat cFilter, src_hsv;
+		cvtColor(src_bgr, src_hsv, CV_RGB2HSV);
+		scalarFilter(src_hsv, cFilter, Scalar(115,120,120), Scalar(125,255,255), false);
+
+		/// depth filter
+		Mat dFilter;
+		scalarFilter(src_dep, dFilter, Scalar(150), Scalar(255), false);
+
+		// DEBUG: display filtered image
+		imshow(windowResult1, src_dep);
+
+		/// ellipse detection
+		RotatedRect eDetect;
+		Point3f currPos, currPosImg, currPosWorld, nextPos, nextPosAll;
+		float nextConf = 0, nextConfAll = 0;
+
+		//TODO: Either store points in world coordinates - estimate position in world and project to camera plane
+		//		OR store points in image coordinates - estimate postition in image plane and project to world
+
+		//TODO: get next estimate with conf. form bb for detection
+		if(detectEllipse(cFilter, eDetect, 150, 7, 7) == 1) {
+			/// get current position - acceleration on the Y axis
+			currPos.x = eDetect.center.x-;	//TODO: optimize
+			currPos.y = eDetect.center.y;
+			currPos.z = src_dep.at<uchar>(eDetect.center.x, eDetect.center.y);
+
+			//TODO: convert coordinates to real world - using camera matrix
+
+			/// estimate the next point
+			currPosWorld = conv.cameraTOworld(currPos);
+			esti.addPoint(currPos);
+			nextConf = esti.estimateNext(nextPos);
+<<<<<<< HEAD
+=======
+			
+			// will work only with world coordinates
+>>>>>>> b2abd9790b5e39a8e033a972a9e0d492604e81a3
+			//nextConfAll = esti.estimateNextAll(nextPosAll);
+
+			//TODO: convert estimated point to image plane
+			currPosImg = conv.worldTOcamera(currPosWorld);
+		}
+		else
+			esti.invalidatePoints();
+
+		/// DEBUG: display detected ellipse - rotated rectangle
+		Point2f rect_points[4]; 
+		eDetect.points(rect_points);
+		for( int j = 0; j < 4; j++ )
+			line(src_bgr, rect_points[j], rect_points[(j + 1) % 4], Scalar(255,0,0), 1, 8);
+		if(nextConf == 1)
+			circle(src_bgr, Point(nextPos.x, nextPos.y), 2, Scalar(0,0,255), -1, 8);
+		if(nextConfAll == 1)
+			circle(src_bgr, Point(nextPosAll.x, nextPosAll.y), 2, Scalar(0,255,0), -1, 8);
+		imshow(windowResult2, src_bgr);
+
+		if(pause1)
+			waitKey(0);
+		else
+			waitKey(10);
+	}
+	return(0);
+}
